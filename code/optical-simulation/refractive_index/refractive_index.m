ded28f7cee31be458f4ddd782ee14fefefe777e3
JunSu Jang <junsu.jang94@gmail.com>
2020-08-19 17:15:37 +0900

    clean up refractive index code

--- code/optical-simulation/refractive_index/refractive_index.m
+++ code/optical-simulation/refractive_index/refractive_index.m
@@ -1,21 +1,37 @@
-n_a = 1;
-n_w = 1.34;
-n_s = 1.75;%1.45; %75;
+% REFRACTIVE INDEX
+% Compute the effective focal length and working distance 
+% based on the refractive indices of the media involved 
+% (i.e., seawater, sapphire and air)
+
+% Refractive index
+n_a = 1; % air
+n_w = 1.34; % water
+n_s = 1.75; % sapphire
+
+% Distance travelled by light (mm)
 d_a = 8;
 d_s = 5;
-fov = deg2rad(8);
-a = 45;
-w = sqrt((2592*0.02)^2 + (1944*0.02/cosd(a))^2);
-% fov = atan((2592*0.0022/2)/25);
-% w = 37.7846*2; %51.84;
 
-h_a = d_a*tan(fov);
+fl = 25; % focal length = 25mm
+% START LED configuration
+% fov = deg2rad(8)
+% in the case of LED, the light is 45deg slanted
+% a = 45;
+% w = sqrt((2592*0.02)^2 + (1944*0.02/cosd(a))^2);
+% END LED
+
+% Lens imaging optical configuration
+% width of the target water volume 
+w = 51.84; % 37.7846*2
+fov = atan((2592*0.0022/2)/fl); % field of view in air
+
+h_a = d_a*tan(fov); % height covered in air
 
 theta_s = asin(n_a/n_s*sin(fov));
-h_s = d_s*tan(theta_s);
-
+h_s = d_s*tan(theta_s); % height covered in sapphire
+ 
 theta_w = asin(n_s/n_w*sin(theta_s));
-h_w = w/2-(h_a+h_s);
-d_w = h_w/tan(theta_w);
-d_w*cosd(a)
-rad2deg(atan(w/2/(d_w+d_s+d_a)))*2
+h_w = w/2-(h_a+h_s); % height covered in water
+d_w = h_w/tan(theta_w);  %distance in water
+d_w % Effective light working distance
+rad2deg(atan(w/2/(d_w+d_s+d_a)))*2 % effective field of view
